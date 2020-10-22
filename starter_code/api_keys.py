66b07a64117d26da8240bffc47681e7bf4b39ef0
Jnajera1 <67034306+Jnajera1@users.noreply.github.com>
2020-10-21 23:34:57 -0400

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "aeb7b6320b45bb216d39cb5f4793e963"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyAEFnGCozmfYoj3oBsfsyrUBRfY5fHVtF8"
+g_key = ""
